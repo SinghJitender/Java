bd6842f0261d3c93a82d1103f86485afe397d620
SinghJitender <jitu.singh95@gmail.com>
2020-02-18 14:03:04 +0530

    Graphs - DijkstrasShortestPath - Testcase Trial

--- src/DataStructures/Graphs/DijkstrasShortestPath.java
+++ src/DataStructures/Graphs/DijkstrasShortestPath.java
@@ -10,11 +10,24 @@ public class DijkstrasShortestPath {
         graph.addEdge(1, 3,10);
         graph.addEdge(2, 3,1);
         graph.addEdge(2, 1,1);
-
         graph.printGraph();
         System.out.println();
         graph.findDijkstrasShortestPath(0);
         System.out.println();
         graph.findDijkstrasShortestPath(2);
+
+        System.out.println("\n\n");
+
+        WeightedGraph graph2 = new WeightedGraph(5);
+        graph2.addEdge(0, 1,4);
+        graph2.addEdge(0, 2,1);
+        graph2.addEdge(2, 1,2);
+        graph2.addEdge(2, 3,4);
+        graph2.addEdge(1, 4,4);
+        graph2.addEdge(3, 4,4);
+        graph2.printGraph();
+        System.out.println();
+        graph2.findDijkstrasShortestPath(0);
+
     }
 }
