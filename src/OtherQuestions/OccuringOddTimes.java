bf856c4bc97a231f78790d46bab7828af7782d85
SinghJitender <jitu.singh95@gmail.com>
2020-02-24 15:26:46 +0530

    OtherQuestions -OccuringOddTimes - Input changes

--- src/OtherQuestions/OccuringOddTimes.java
+++ src/OtherQuestions/OccuringOddTimes.java
@@ -7,7 +7,9 @@ package OtherQuestions;
 
 public class OccuringOddTimes {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,2,3,1,3,3};
+        int arr[] = {1,2,3,2,3,1,3};
+        //int arr[] = {1,2,3,2,3,1,3,3};
+        //int arr[] = {1,2,3,2,3,1,3,3,2};
         int result = 0;
         for(int i=0;i<arr.length;i++) {
             result ^= arr[i];
