344ee4123c580b95cbb79ce5a1efcf65923da722
SinghJitender <Jitu.singh95@gmail.com>
2020-02-28 00:58:47 +0530

    Algorithms/String - FiniteAutomata_BuildStateMachine

---
+++ src/Algorithms/Strings/FiniteAutomata_BuildStateMachine.java
@@ -0,0 +1,12 @@
+package Algorithms.Strings;
+
+public class FiniteAutomata_BuildStateMachine {
+  public static void main(String[] args) {
+    String pattern = "ababaca";
+
+    for(int i=0;i<pattern.length();i++) {
+
+    }
+
+  }
+}
